8b460d3f9ad40eb852c0de11b5bb7598163c4e5a
Patricia Disaign <patriciandrea.p@gmail.com>
2017-01-06 17:38:57 -0300

    Ejercicio 2

--- js/ejercicio-1.js
+++ js/ejercicio-1.js
@@ -1,2 +1,29 @@
-$(document).ready( function(){});
+// $(document).ready( function(){});
 
+function transformTime (hora){
+	var input = hora;
+	var result = "";
+
+	if ((input.length == 10) && (parseInt(input.substr(0,2)) >= 0) && (parseInt(input.substr(0,2)) <= 12) && (parseInt(input.substr(3,2)) >= 0) && (parseInt(input.substr(3,2)) <= 60) && (parseInt(input.substr(6,2)) >= 0) && (parseInt(input.substr(6,2))<= 60) ){ 
+		if (input.charAt(8) == 'A') {
+
+			result = input.slice(0,8);
+			var num = parseInt(input.substr(0,2));
+
+			if( num === 12){
+				num = "00";
+			}
+
+			result = num + input.slice(2,8);
+			alert(result);
+		}
+		else{
+			num = parseInt(input.substr(0,2));
+			result = num + input.slice(2,8);
+			alert(result);
+		}
+	}
+	else {
+		alert("Ingresa una hora correcta Ej. 06:55:44AM");
+	}
+}	
--- js/ejercicio-2.js
+++ js/ejercicio-2.js
@@ -1,2 +1,26 @@
-$(document).ready( function(){});
+function hashtag_ (n){
+	var num = parseInt(n);
+	var result = "";
+
+	for ( i = 0 ; i < num ; i++){
+		var hash = "#";
+		var espacio = "  ";
+		var cont = 1;
+		for ( j = 0 ; j < num ; j++){
+
+			if (cont !== (num-i)){
+				cont ++;
+				result += espacio;
+			}
+			else{
+				result += hash;
+			}
+		}
+
+		if ( i != num-1){
+			result += "\n";
+		}
+	}
+	alert(result);
+}
 
